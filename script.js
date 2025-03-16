a8fb7f57c590f5b50597561749ab7a5861d9b827
Namdev23 <nguyennamtiger@gmail.com>
2025-03-17 00:04:44 +0700

    OK

--- script.js
+++ script.js
@@ -9,16 +9,7 @@ const express = require('express');
 const app = express();
 
 app.post('/paddle-webhook', async (req, res) => {
-  const { subscription_id, event_type } = req.body;
-
-  if (event_type === 'subscription_cancelled') {
-    const userDoc = await db.collection('users').where('uid', '==', subscription_id).get();
-    
-    if (!userDoc.empty) {
-      const userRef = userDoc.docs[0].ref;
-      await userRef.update({ plan: "lite" });
-    }
-  }
+  console.log(req.body);
 
   res.sendStatus(200);
 })
