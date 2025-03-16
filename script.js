0de5fc5b2f222a53881aa443ed3a132bff20c8af
Namdev23 <nguyennamtiger@gmail.com>
2025-03-17 00:10:48 +0700

    Changed

--- script.js
+++ script.js
@@ -9,7 +9,25 @@ const express = require('express');
 const app = express();
 
 app.post('/paddle-webhook', async (req, res) => {
-  console.log(req.body);
+  const { custom_data, event_type } = req.body;
+  custom_data = JSON.parse(custom_data);
+
+  if (event_type === 'subscription_cancelled') {
+    const userDoc = await db.collection('users').where('uid', '==', custom_data.uid).get();
+    
+    if (!userDoc.empty) {
+      const userRef = userDoc.docs[0].ref;
+
+      try {
+        await userRef.setDoc({
+          plan: 'lite',
+        }, { merge: true })
+      }
+      catch (error) {
+        console.error(error)
+      }
+    }
+  }
 
   res.sendStatus(200);
 })
