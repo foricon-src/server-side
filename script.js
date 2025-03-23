5c87cc95d1f3133f97e5ec558eedc341a019f22d
Namdev23 <nguyennamtiger@gmail.com>
2025-03-23 20:29:37 +0700

    5

--- script.js
+++ script.js
@@ -136,6 +136,6 @@ async function checkAndSyncEmails() {
         console.error("Error checking and syncing emails:", error);
     }
 }
-setInterval(checkAndSyncEmails, 10000);
+setInterval(checkAndSyncEmails, 5000);
 
 app.listen(3000, () => console.log('Server running on port 3000'));
